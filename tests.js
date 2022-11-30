4d62474610c3346224bd790c03a1de16fdbc1a96
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-11-30 16:23:48 -0600

    Step 1 complete.

--- tests.js
+++ tests.js
@@ -13,3 +13,18 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe("function");
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Say hello!" when executed', function () {
+        expect(sayHello()).toBe("Say hello!");
+    });
+    it("should never return 'undefined' when called", function () {
+        expect(sayHello()).not.toBe(undefined);
+    });
+});

47b904e1ba64f8519de27a88567bc8de54c2c49f
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-12-01 22:12:58 -0600

    Step 1 complete.

--- tests.js
+++ tests.js
@@ -30,7 +30,7 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!" when executed', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-    // it('should never return "undefined" when called ', function () {
-    //     expect(sayHello()).not.toBe(undefined);
-    // });
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(sayHello("World")).toBe("Hello, World!")
+    });
 });

1d57a5d8eef056fa44212254a68698fe44fac9af
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-12-03 20:47:57 -0600

    Step 1- 8 complete.

--- code.js
+++ code.js
@@ -2,11 +2,12 @@ function helloWorld(){
     return "Hello, World!";
 }
 
+let noGreet = [ Number, null, ""]
 
 function sayHello(input) {
     if (input === true || input === false) {
         return "Hello, Boolean!"
-    } else if (input === Number || input === null) {
+    } else if (input === noGreet) {
         return "You're a bot!"
     } else {
             return "Hello, " + input + "!";
